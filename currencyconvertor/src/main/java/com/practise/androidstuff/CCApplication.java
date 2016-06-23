4533620ac6e76cbcc4b7e0a06bf4a0ff9455af0c
Wahib <wahib.tech@gmail.com>
2016-06-23 23:44:18 +0200

    BaseFragment and BaseApplication added

---
+++ currencyconvertor/src/main/java/com/practise/androidstuff/CCApplication.java
@@ -0,0 +1,7 @@
+package com.practise.androidstuff;
+
+import android.app.Application;
+
+public class CCApplication extends Application {
+
+}
---
+++ currencyconvertor/src/main/java/com/practise/androidstuff/views/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.practise.androidstuff.views;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Base class for all fragments to introduce common
+ * functionality.
+ */
+public abstract class BaseFragment extends Fragment {
+}
